7cfb0d57dff36904261cd62792cce69ec5420c1b
Cloud! <67177891+thecloudywolf@users.noreply.github.com>
2023-03-01 08:07:02 -0800

    Fixed Fire.cs

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -3,33 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fire : MonoBehaviour
-{ 
-    public Rigidbody projectile;
-public float speed = 1190;
-    public double r = -1.5;
-    Rigidbody instantiatedProjectile;
-    void OnTriggerEnter(Collider other)
-    {
-        
-
-
-
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-
-        
-    }
-    // Update is called once per frame
-    void Update()
 {
-    if (Input.GetButtonDown("Fire1"))
+    public Rigidbody projectile;
+    public float speed = 1190f;
+    public float r = -0.2f;
+    public float angle = 0f;
+    public GameObject[] arrr;
+
+
+    void Update()
     {
-         instantiatedProjectile = Instantiate(projectile, transform.position + new Vector3(0,(float)r,0) , transform.rotation) as Rigidbody;
-        instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed * 4));
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            speed = 1190f;
+            r = -1.5f;
+            Rigidbody instantiatedProjectile;
+
+        }
     }
-}
 
 
-   
+
+
+
+
+
+    void OnTriggerEnter(Collider other)
+
+    {
+        for (int i = 0; i < 312; i++)
+        {
+            Rigidbody instantiatedProjectile;
+
+            instantiatedProjectile = Instantiate(projectile, transform.position + new Vector3(0, (float)r + (-1), 0), transform.rotation) as Rigidbody;
+            instantiatedProjectile.transform.Rotate(new Vector3(Random.Range(-15, 15), Random.Range(-angle, angle), Random.Range(-angle, angle)));
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed * 4));
+
+
+
+            instantiatedProjectile.velocity = instantiatedProjectile.transform.TransformDirection(new Vector3(0, 0, speed * 4));
+
+        }
+    }
+
 }
