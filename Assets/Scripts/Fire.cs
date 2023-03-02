692f833fa69de8e7849758d5572fbf74dcebfd82
Bball6805 <112641577+Bball6805@users.noreply.github.com>
2023-03-02 08:34:13 -0800

    test(push)
    wffawfwa

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -1,51 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Fire : MonoBehaviour
 {
     public Rigidbody projectile;
+    Rigidbody instantiatedProjectile;
     public float speed = 1190f;
     public float r = -0.2f;
-    public float angle = 0f;
+    public float angle = 90f;
     public GameObject[] arrr;
-
-
-    void Update()
-    {
-
-        if (Input.GetButtonDown("Fire1"))
-        {
-
-            speed = 1190f;
-            r = -1.5f;
-            Rigidbody instantiatedProjectile;
-
-        }
-    }
-
-
-
-
 
 
 
     void OnTriggerEnter(Collider other)
-
     {
-        for (int i = 0; i < 312; i++)
+
+
+        Destroy(instantiatedProjectile.gameObject);
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
         {
-            Rigidbody instantiatedProjectile;
+           for (int r = 0; r < 312; r++)
+                {
+                    Rigidbody instantiatedProjectile;
 
-            instantiatedProjectile = Instantiate(projectile, transform.position + new Vector3(0, (float)r + (-1), 0), transform.rotation) as Rigidbody;
-            instantiatedProjectile.transform.Rotate(new Vector3(Random.Range(-15, 15), Random.Range(-angle, angle), Random.Range(-angle, angle)));
-            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed * 4));
+                    instantiatedProjectile = Instantiate(projectile, transform.position + new Vector3(0, (float)r + (-1), 0), transform.rotation) as Rigidbody;
+                    instantiatedProjectile.transform.Rotate(new Vector3(Random.Range(-15, 15), Random.Range(-angle, angle), Random.Range(-angle, angle)));
+                //    instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed * 4));
+                    
+
+                    instantiatedProjectile.velocity = instantiatedProjectile.transform.TransformDirection(new Vector3(0, 0, speed * 4));
+
+                }
+            
 
 
 
-            instantiatedProjectile.velocity = instantiatedProjectile.transform.TransformDirection(new Vector3(0, 0, speed * 4));
 
         }
     }
-
 }
